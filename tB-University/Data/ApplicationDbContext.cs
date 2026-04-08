46d274ce5853ab681a68aa566a685de9b7da1415
Joao Silva <jotal96@gmail.com>
2026-04-08 10:42:46 +0100

    criada a migração e feito o update na base de dados

--- tB-University/Data/ApplicationDbContext.cs
+++ tB-University/Data/ApplicationDbContext.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using tB_University.Models;
 
 namespace tB_University.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    // tabelas relacionados com cursos e UCs respetivamente
+    public DbSet<Degree> Degrees { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    
+    // tabelas de utilizadores
+    public DbSet<MyUser> MyUsers { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    
+    
+    // tabelas de relacionamentos
+    public DbSet<Registration> Registrations { get; set; }
 }
--- tB-University/Data/Migrations/20260408093522_initDb.Designer.cs
+++ tB-University/Data/Migrations/20260408093522_initDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using tB_University.Data;
 
@@ -10,9 +11,11 @@ using tB_University.Data;
 namespace tB_University.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260408093522_initDb")]
+    partial class initDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
