6b0c9920a80cc09690ba89a43aaee6ea9e0ccf2e
Joao Silva <jotal96@gmail.com>
2026-04-22 09:36:02 +0100

    Alteração para no Create ser usado formato yyyy/yyyy

--- tB-University/Pages/Courses/Create.cshtml.cs
+++ tB-University/Pages/Courses/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -28,13 +29,21 @@ namespace tB_University.Pages.Courses
         [BindProperty]
         public Course Course { get; set; } = default!;
 
+        [BindProperty]
+        [Display(Name = "Ano da Unidade Curricular")]
+        [RegularExpression("[1-2][0-9]{3}[/][1-2][0-9]{3}", ErrorMessage = "O {0} deve seguir o formato yyyy/yyyy.")]
+        public string? AnoAux { get; set; } = string.Empty;
+
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
+                ViewData["DegreeFk"] = new SelectList(_context.Degrees, "Id", "Name");
                 return Page();
             }
+            var AnoAuxArray =  AnoAux?.Split('/');
+            Course.CurricularYear = Convert.ToInt32(AnoAuxArray[0]);
 
             _context.Courses.Add(Course);
             await _context.SaveChangesAsync();
